77c5311e1c46536577e111f1cf9211f0db33d70f
akshayhangloo <hangloo.akshay23@gmail.com>
2017-07-17 18:21:26 +0530

    Update client_logic.js

--- client_logic.js
+++ client_logic.js
@@ -1,1 +1,3 @@
-
+if (!('serviceWorker' in navigator)) { console.log("Service Workers aren't supported on this browser"); return; } 
+  
+if (!('PushManager' in window)) {  console.log("Push messaging isn't supported on this browser"); return; }
